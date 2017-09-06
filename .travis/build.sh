8c30c5627cd9b31cc4c36d11b73d39cc68a1507b
Ulf Lilleengen <lulf@redhat.com>
2017-09-06 14:23:01 +0200

    Run only QueueTest

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,8 +3,8 @@ set -e
 export TAG=${TRAVIS_TAG:-latest}
 export VERSION=${TRAVIS_TAG:-latest}
 
-./gradlew build -i
+./gradlew build -i -x test
 
-./gradlew pack buildImage tagImage pushImage
+./gradlew pack buildImage tagImage pushImage -x test
 ./systemtests/scripts/run_test_component.sh templates/install /tmp/openshift systemtests
 ./.travis/generate-bintray-descriptor.sh enmasse templates/build/enmasse-${TAG}.tgz > .bintray.json
